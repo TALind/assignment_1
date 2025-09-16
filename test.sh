2fb837487db8268f6f29e628cb744904f1c6a61a
Camellia <s203237@student.dtu.dk>
2025-09-17 00:25:35 +0200

    add test cases

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 
 # Simple test of the command interpreter
 
@@ -7,4 +7,13 @@ out="0,3,5;"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
+# # test case 1: only "a" and "b"
+# in="ababaq"
+# out="0,2,4;"
+# [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "test case 1 : PASSED" || echo "test case 1: FAILED"
 
+
+# #test case 2: using "c" to remove previous element
+# in="abcabcabq"
+# out="6;"
+# [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "test case 2 : PASSED" || echo "test case 2: FAILED"
